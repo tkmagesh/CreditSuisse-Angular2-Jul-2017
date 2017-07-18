dad10e8b18c19ffe4a33148ebb801f9022721961
Magesh <tkmagesh77@gmail.com>
2017-07-18 14:40:37 +0800

    Adding and listing bugs

--- bug-tracker-app/src/app/bug-tracker/bugTracker.component.ts
+++ bug-tracker-app/src/app/bug-tracker/bugTracker.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
+interface IBug{
+	name : string,
+	isClosed : boolean,
+	createdAt : Date
+}
+
+
 @Component({
 	selector : 'bug-tracker',
 	templateUrl : 'bugTracker.component.html',
 	styleUrls : ['bugTracker.component.css']
 })
 export class BugTrackerComponent{
-	
+	bugs : IBug[] = [];
+	onCreateClick(newBugName : string){
+		let newBug : IBug = {
+			name : newBugName,
+			isClosed : false,
+			createdAt : new Date()
+		};
+		this.bugs.push(newBug);
+	}
 }
